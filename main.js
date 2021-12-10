625db6e56b4ea7a1f0c3844ad3eeb99b41b3fa1f
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-10 10:00:20 +0100

    chiusura - apertura

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const hamburger = document.querySelector(".fa-bars");
 const hamburgerClose = document.querySelector(".fa-times");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 
+// apertura 
 hamburger.addEventListener("click", function() {
   hamburgerMenu.classList.toggle("active");
 });
 
+// chiusura
 hamburgerClose.addEventListener("click", function(){
   hamburgerMenu.classList.remove("active");
 });
