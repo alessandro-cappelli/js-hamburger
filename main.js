d9c52ca44a0fd275a0ec6a0cd2b15b38eba5437f
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-10 09:53:14 +0100

    chiusura hamburger

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const hamburger = document.querySelector(".fa-bars");
+const hamburgerClose = document.querySelector(".fa-times");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 
-hamburger.addEventListener("click",() => {
-  hamburger.classList.toggle("active");
+hamburger.addEventListener("click", function() {
   hamburgerMenu.classList.toggle("active");
-})
+});
+
+hamburgerClose.addEventListener("click", function(){
+  hamburgerMenu.classList.remove("active");
+});
